38381e933db938f3e2d6ca6a63150ff2181537a5
alank <73473867+ruslan2321@users.noreply.github.com>
2026-05-16 12:04:42 +0300

    Config BE

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './profile/user.entities';
 import { UsersModule } from './profile/user.module';
-import { JwtModule } from '@nestjs/jwt';
-import { Card } from './card/card.entites';
 import { CardModule } from './card/card.module';
 import { MailerModule } from './mailer/mailer.module';
 import { ResetPasswordModule } from './reset-password/reset-password.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      username: 'postgres',
-      port: 5432,
-      host: 'localhost',
-      password: 'ruslan',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: ['users', 'cards'],
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        ssl: true,
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
 
-    TypeOrmModule.forFeature([User, Card]),
+    JwtModule,
     UsersModule,
     CardModule,
-    JwtModule,
     MailerModule,
     ResetPasswordModule,
   ],
