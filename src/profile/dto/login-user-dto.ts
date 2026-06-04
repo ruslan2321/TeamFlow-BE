af3f30b1d3c38157acb7f5541b433697df9ba26a
alank <73473867+ruslan2321@users.noreply.github.com>
2026-06-04 10:48:20 +0300

    TFB-6

--- src/profile/dto/login-user-dto.ts
+++ src/profile/dto/login-user-dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class login {
+  @IsString({ message: 'Логин должен быть строкой' })
+  @IsNotEmpty({ message: 'Логин обязателен' })
   login!: string;
+
+  @IsString({ message: 'Пароль должен быть строкой' })
+  @IsNotEmpty({ message: 'Пароль обязателен' })
   password!: string;
 }
