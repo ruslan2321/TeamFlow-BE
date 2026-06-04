8a2b6543861d8ec17fc06da22a602a0edd20256b
alank <73473867+ruslan2321@users.noreply.github.com>
2026-06-04 09:57:57 +0300

    TF-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,50 +1,40 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './profile/user.entities';
 import { UsersModule } from './profile/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { Card } from './card/card.entites';
 import { CardModule } from './card/card.module';
 import { MailerModule } from './mailer/mailer.module';
 import { ResetPasswordModule } from './reset-password/reset-password.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const databaseUrl = config.get<string>('DATABASE_URL');
-
-        if (!databaseUrl) {
-          throw new Error('DATABASE_URL is missing');
-        }
-
-        return {
-          type: 'postgres',
-          url: databaseUrl,
-          ssl: { rejectUnauthorized: false },
-          autoLoadEntities: true,
-          synchronize: false,
-        };
-      },
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.getOrThrow<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
     }),
 
-    JwtModule.register({}),
+    TypeOrmModule.forFeature([User, Card]),
     UsersModule,
     CardModule,
+    JwtModule,
     MailerModule,
     ResetPasswordModule,
   ],
+  exports: [JwtModule],
   controllers: [AppController],
   providers: [AppService],
-  exports: [JwtModule],
 })
 export class AppModule {}
