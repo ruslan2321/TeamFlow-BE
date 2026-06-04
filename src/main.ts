44099bb93ed30c4a6a6136a2bd970f421ac8b9cc
alank <73473867+ruslan2321@users.noreply.github.com>
2026-06-04 10:05:47 +0300

    TF-4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
-import { createApp } from './bootstrap';
+import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await createApp();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 
